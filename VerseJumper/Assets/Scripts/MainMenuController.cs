e05f0c5f44b6114e5f637fa1c2c3b391c2d7d9a7
Noah Walker <93171964+Sopro7727@users.noreply.github.com>
2023-11-01 18:27:51 -0700

    Created Main Menu

---
+++ VerseJumper/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    // Start is called before the first frame update
+    public void playGame(){
+        SceneManager.LoadScene("Level 1");
+    }
+    public void options(){
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void back(){
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+    public void credits(){
+        SceneManager.LoadScene("Credits");
+    }
+    public void level1(){
+        SceneManager.LoadScene("Level 1");
+    }
+    public void level2(){
+        SceneManager.LoadScene("Level 2");
+    }
+    public void levelBack(){
+        SceneManager.LoadScene("Main Menu");
+    }
+}
