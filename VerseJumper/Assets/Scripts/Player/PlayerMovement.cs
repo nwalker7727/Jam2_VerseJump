678872954edde9dc7a0d4b07d8abda31afbfaf7e
Justin Vicente <javicent@ucsc.edu>
2023-11-01 17:50:41 -0700

    Merge Justin's Branch

---
+++ VerseJumper/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.9f, groundLayer);
+        Debug.Log("Is Grounded: " + isGrounded);
+
+        // Player movement
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Jumping
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
+
+
