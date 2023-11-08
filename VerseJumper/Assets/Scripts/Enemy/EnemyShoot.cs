b46ff8b6c3ad351b1fbfd79c06305a244e5cf353
mmaaddiiiee <52755121+mmaaddiiiee@users.noreply.github.com>
2023-11-07 19:14:32 -0800

    level 3 edit

--- VerseJumper/Assets/Scripts/Enemy/EnemyShoot.cs
+++ VerseJumper/Assets/Scripts/Enemy/EnemyShoot.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EnemyShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject projectilePrefab; // Reference to the projectile prefab.
+    public float shootInterval = 1.0f; // Time interval between shots.
+    public float shootSpeed = 5.0f; // Speed of the projectile.
+    public Vector2 shootDirection = Vector2.right; // Shooting direction.
+
+    private void Start()
     {
-        
+        // Start shooting projectiles when the enemy is created.
+        StartCoroutine(ShootProjectiles());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShootProjectiles()
     {
-        
+        while (true)
+        {
+            // Create a new projectile instance.
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+            // Set the projectile's initial velocity based on the shootDirection.
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.velocity = shootDirection.normalized * shootSpeed;
+
+            // Destroy the projectile after a certain time if it doesn't hit anything.
+            Destroy(projectile, 5.0f);
+
+            // Wait for the next shot based on the specified shootInterval.
+            yield return new WaitForSeconds(shootInterval);
+        }
     }
 }
